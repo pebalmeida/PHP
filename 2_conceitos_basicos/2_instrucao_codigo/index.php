1f1d76ea2ef80ff37691d7eca92ea5b084e08d7b
Pericles Belo de Almeida <93488924+pebalmeida@users.noreply.github.com>
2024-11-22 14:05:42 -0300

    Add files via upload

---
+++ 2_conceitos_basicos/2_instrucao_codigo/index.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "uma linha <br>";
+echo "Outra linha <br>";
+
+
+if(9 > 4){
+    echo "Dentro do if";
+}
+
+
+
+
+
+?>
---
+++ 3_tipos_dados/1_Inteiros/index.php
@@ -0,0 +1,28 @@
+<?php
+
+echo 1000;
+echo "<br>";
+echo 25 + 34;
+echo "<br>";
+echo -15;
+echo "<br>";
+echo 35;
+echo "<br>";
+echo -5000 + 3200;
+echo "<br>";
+
+
+$y = 3;
+
+echo "<br>";
+echo $y;
+
+
+
+
+
+
+
+
+
+?>
